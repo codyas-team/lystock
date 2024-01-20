648323d29a669dd58626f630bcbe9e912d02696f
Fernando Sera G <fsera@xetid.cu>
2024-01-19 23:11:45 -0500

    Correción en Importar Artículo

--- resources/lang/es-ES/import.php
+++ resources/lang/es-ES/import.php
@@ -6,5 +6,8 @@ return [
     'title'                 => 'Importar :type',
     'limitations'           => 'Tipos de archivo permitidos: :extensiones<br>Permitido máximo de filas: :row_limit',
     'sample_file'           => 'Puedes <a target="_blank" href=":download_link"><strong>descargar</strong></a> el archivo de ejemplo y rellenarlo con tus datos.',
+    'sample_file_and_document'  => 'Puede <a target="_blank" href=":download_link" class="text-blue-500 underline font-bold">descargar</a> el archivo de muestra y revisar los <a target="_blank" href=":document_link"class="text-blue-500 underline font-bold">documentos</a> para obtener información detallada',
+    'drop_file'                 => '<span style="color: #006EA6;">Sube un archivo</span> o arrastra y suelta',
+    'file_type_and_limitations' => ':extensions hasta :row_limit filas',
 
 ];
